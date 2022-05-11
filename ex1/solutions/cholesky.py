d2e758809d6ab533dc859da51f452517d68dd845
pedro kuchpil <pedrokuchpil@gmail.com>
2022-05-11 01:34:38 -0300

    Add cholesky decomposition, second task

--- ex1/solutions/cholesky.py
+++ ex1/solutions/cholesky.py
@@ -1,5 +1,29 @@
 from ex1.solutions.solution import Solution
+from utils.matrix import get_transposed_matrix
 
 class CholeskySolution(Solution):
+  def decompose(self):
+    n = len(self.A)
+    m = len(self.A[0])
+
+    if n != m:
+      raise Exception("ERRO: Matriz não é quadrada")
+
+    L = [[0.0] * n for i in range(n)]
+
+    for i in range(n):
+      for j in range(i+1):
+        tmp = sum(L[i][k] * L[j][k] for k in range(j))
+
+        if i == j:
+          L[i][j] = (self.A[i][j] - tmp)**0.5
+        else:
+          L[i][j] = (1.0 / L[j][j] * (self.A[i][j] - tmp))
+      
+      return L
+
   def solve(self):
-    pass
+    L = self.decompose()
+    LT = get_transposed_matrix(L)
+
+    #Incomplete
