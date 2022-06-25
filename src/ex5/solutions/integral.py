10120794d684a8d4fda8c841ce30ee2ddc89380a
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-06-25 19:45:56 -0300

    add gauss quadrature solution!

--- src/ex5/solutions/integral.py
+++ src/ex5/solutions/integral.py
@@ -1,5 +1,6 @@
 from src.ex5.solutions.solution import Solution
-from src.ex5.utils.quadratures import get_polynomial_quadrature
+from src.ex5.utils.polynomial_quadrature import get_polynomial_quadrature
+from src.ex5.utils.gauss_quadrature import get_gauss_legendre_quadrature
 
 class Integral(Solution):
   def __init__(self, c1, c2, c3, c4, **kwargs):
@@ -14,7 +15,11 @@ class Integral(Solution):
     if self.number_of_integration_points < 2 or self.number_of_integration_points > 10: raise RuntimeError('Número de pontos de integração inválido.') 
 
   def gauss_legendre(self):
-    pass
+    quadrature = get_gauss_legendre_quadrature(self.number_of_integration_points)
+    L = self.b - self.a
+    points = map(lambda point: 0.5*(self.a + self.b + point*L), quadrature.get('points'))
+    sum_pw = sum([self.F(point) * weight for point, weight in zip(points, quadrature['weights'])])
+    return (L/2)*sum_pw
 
   def polynomial_quadrature(self):
     points, weights = get_polynomial_quadrature(self.number_of_integration_points, self.a, self.b)
